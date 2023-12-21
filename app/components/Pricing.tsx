ae3e65c071afd63cf257c1b67d2a608aa8d3c79a
Darrell-Rafa <drafa0609@gmail.com>
2023-12-21 22:38:09 +0700

    finish pricing

---
+++ app/components/Pricing.tsx
@@ -0,0 +1,152 @@
+import Image from "next/image";
+import React from "react";
+import { lora } from "../fonts";
+
+function Pricing() {
+  return (
+    <section id="pricing" className="px-6 lg:px-28 py-40">
+      <h1
+        className={`text-center text-3xl leading-snug font-semibold ${lora.className} mb-4`}
+      >
+        Pricing Plans
+      </h1>
+      <p className="text-[#7C7C7C] font-medium text-lg text-center mb-16 lg:mb-20">
+        Customize your wedding journey with pricing plans that suit you best.
+      </p>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-6">
+        <div className="p-6 bg-[#F66F6F] rounded space-y-10 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] text-white lg:order-last">
+          <button className="border-[1.5px] border-white rounded px-4 py-2 text-lg">
+            Priority
+          </button>
+          <h1 className="text-4xl font-medium">Rp10.000.000</h1>
+          <div className="space-y-4">
+            <div className="flex items-center gap-4">
+              <Image
+                src="/assets/tick-pricing.svg"
+                alt="benefit"
+                width={24}
+                height={24}
+              />
+              <span>Profesional Live Streaming</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <Image
+                src="/assets/tick-pricing.svg"
+                alt="benefit"
+                width={24}
+                height={24}
+              />
+              <span>Video Call with 40 inch TV</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <Image
+                src="/assets/tick-pricing.svg"
+                alt="benefit"
+                width={24}
+                height={24}
+              />
+              <span>Dedicated Internet</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <Image
+                src="/assets/tick-pricing.svg"
+                alt="benefit"
+                width={24}
+                height={24}
+              />
+              <span>360 Degree Documentation</span>
+            </div>
+          </div>
+          <button className="w-full text-xl border-2 border-black bg-black rounded text-center font-medium py-4">
+            Get Started
+          </button>
+        </div>
+        <div className="p-6 bg-[#FFFFF0] rounded space-y-10 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+          <button className="border-[1.5px] border-black rounded px-4 py-2 text-lg">
+            Premium
+          </button>
+          <h1 className="text-4xl font-medium">Rp2.000.000</h1>
+          <div className="space-y-4 lg:space-y-0 lg:grid grid-cols-2 gap-4">
+            <div className="flex items-center gap-4">
+              <Image
+                src="/assets/tick-pricing.svg"
+                alt="benefit"
+                width={24}
+                height={24}
+              />
+              <span>Digital Invitation (1000 Guests)</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <Image
+                src="/assets/tick-pricing.svg"
+                alt="benefit"
+                width={24}
+                height={24}
+              />
+              <span>Virtual Reseption</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <Image
+                src="/assets/tick-pricing.svg"
+                alt="benefit"
+                width={24}
+                height={24}
+              />
+              <span>Wedding Photo Gallery</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <Image
+                src="/assets/tick-pricing.svg"
+                alt="benefit"
+                width={24}
+                height={24}
+              />
+              <span>Video Call</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <Image
+                src="/assets/tick-pricing.svg"
+                alt="benefit"
+                width={24}
+                height={24}
+              />
+              <span>Wedding Live Streaming</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <Image
+                src="/assets/tick-pricing.svg"
+                alt="benefit"
+                width={24}
+                height={24}
+              />
+              <span>Digital Gift</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <Image
+                src="/assets/tick-pricing.svg"
+                alt="benefit"
+                width={24}
+                height={24}
+              />
+              <span>Souvenir Delivery</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <Image
+                src="/assets/tick-pricing.svg"
+                alt="benefit"
+                width={24}
+                height={24}
+              />
+              <span>1 year access</span>
+            </div>
+          </div>
+          <button className="w-full text-xl border-2 border-black rounded text-center font-medium py-4">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Pricing;
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import GeneralFeatures from "./components/GeneralFeatures";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import MobileNavbar from "./components/MobileNavbar";
+import Pricing from "./components/Pricing";
 import SpecificFeatures from "./components/SpecificFeatures";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <Award />
       <GeneralFeatures />
       <SpecificFeatures />
+      <Pricing />
     </>
   );
 }
