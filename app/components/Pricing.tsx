82157bdced9d318f2687fea24471105a5dfb410d
Darrell-Rafa <drafa0609@gmail.com>
2024-01-06 23:28:24 +0700

    update sumn

--- app/components/Pricing.tsx
+++ app/components/Pricing.tsx
@@ -29,53 +29,78 @@ function Pricing({
           {dict.subheading}
         </p>
       </Fade>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-6 mb-12 lg:h-[40.5rem]">
         <Fade direction="left">
-          <div className="p-6 bg-[#F66F6F] rounded space-y-10 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] text-white lg:order-last">
-            <button className="border-[1.5px] border-white rounded px-4 py-2 text-lg">
-              Priority
-            </button>
-            <h1 className="text-4xl font-medium">Rp10.000.000</h1>
-            <div className="space-y-4">
-              <div className="flex items-center gap-4">
-                <Image
-                  priority={true}
-                  src="/assets/tick-pricing.svg"
-                  alt="benefit"
-                  width={24}
-                  height={24}
-                />
-                <span>Profesional Live Streaming</span>
-              </div>
-              <div className="flex items-center gap-4">
-                <Image
-                  priority={true}
-                  src="/assets/tick-pricing.svg"
-                  alt="benefit"
-                  width={24}
-                  height={24}
-                />
-                <span>Video Call with 40 inch TV</span>
-              </div>
-              <div className="flex items-center gap-4">
-                <Image
-                  priority={true}
-                  src="/assets/tick-pricing.svg"
-                  alt="benefit"
-                  width={24}
-                  height={24}
-                />
-                <span>Dedicated Internet</span>
-              </div>
-              <div className="flex items-center gap-4">
-                <Image
-                  priority={true}
-                  src="/assets/tick-pricing.svg"
-                  alt="benefit"
-                  width={24}
-                  height={24}
-                />
-                <span>360 Degree Documentation</span>
+          <div className="p-6 bg-[#F66F6F] rounded shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] text-white lg:order-last lg:h-full lg:flex flex-col justify-between items-baseline">
+            <div className="space-y-10 mb-10 lg:mb-0">
+              <button className="border-[1.5px] border-white rounded px-4 py-2 text-lg">
+                Lite
+              </button>
+              <h1 className="text-4xl font-medium">Rp400.000</h1>
+              <div className="space-y-4">
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>
+                    Digital Invitations for 200 Guests, Sent via Virtuwed's
+                    Official Account
+                  </span>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>Premium Design</span>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>360-Degree Virtual Reception</span>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>360-Degree Virtual Memories</span>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>Digital Gifts</span>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>1-Year Access</span>
+                </div>
               </div>
             </div>
             <button className="w-full text-xl border-2 border-black bg-black rounded text-center font-medium py-4">
@@ -84,91 +109,96 @@ function Pricing({
           </div>
         </Fade>
         <Fade direction="right">
-          <div className="p-6 bg-[#FFFFF0] rounded space-y-10 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
-            <button className="border-[1.5px] border-black rounded px-4 py-2 text-lg">
-              Premium
-            </button>
-            <h1 className="text-4xl font-medium">Rp2.000.000</h1>
-            <div className="space-y-4 lg:space-y-0 lg:grid grid-cols-2 gap-4">
-              <div className="flex items-center gap-4">
-                <Image
-                  priority={true}
-                  src="/assets/tick-pricing.svg"
-                  alt="benefit"
-                  width={24}
-                  height={24}
-                />
-                <span>Digital Invitation (1000 Guests)</span>
-              </div>
-              <div className="flex items-center gap-4">
-                <Image
-                  priority={true}
-                  src="/assets/tick-pricing.svg"
-                  alt="benefit"
-                  width={24}
-                  height={24}
-                />
-                <span>Virtual Reseption</span>
-              </div>
-              <div className="flex items-center gap-4">
-                <Image
-                  priority={true}
-                  src="/assets/tick-pricing.svg"
-                  alt="benefit"
-                  width={24}
-                  height={24}
-                />
-                <span>Wedding Photo Gallery</span>
-              </div>
-              <div className="flex items-center gap-4">
-                <Image
-                  priority={true}
-                  src="/assets/tick-pricing.svg"
-                  alt="benefit"
-                  width={24}
-                  height={24}
-                />
-                <span>Video Call</span>
-              </div>
-              <div className="flex items-center gap-4">
-                <Image
-                  priority={true}
-                  src="/assets/tick-pricing.svg"
-                  alt="benefit"
-                  width={24}
-                  height={24}
-                />
-                <span>Wedding Live Streaming</span>
-              </div>
-              <div className="flex items-center gap-4">
-                <Image
-                  priority={true}
-                  src="/assets/tick-pricing.svg"
-                  alt="benefit"
-                  width={24}
-                  height={24}
-                />
-                <span>Digital Gift</span>
-              </div>
-              <div className="flex items-center gap-4">
-                <Image
-                  priority={true}
-                  src="/assets/tick-pricing.svg"
-                  alt="benefit"
-                  width={24}
-                  height={24}
-                />
-                <span>Souvenir Delivery</span>
-              </div>
-              <div className="flex items-center gap-4">
-                <Image
-                  priority={true}
-                  src="/assets/tick-pricing.svg"
-                  alt="benefit"
-                  width={24}
-                  height={24}
-                />
-                <span>1 year access</span>
+          <div className="p-6 bg-[#FFFFF0] rounded shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] lg:h-full lg:flex flex-col justify-between items-baseline">
+            <div className="space-y-10 mb-10 lg:mb-0">
+              <button className="border-[1.5px] border-black rounded px-4 py-2 text-lg">
+                Priority
+              </button>
+              <h1 className="text-4xl font-medium">Rp5.000.000</h1>
+              <div className="space-y-4">
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>
+                    Digital Invitations for 200 Guests, Sent via Virtuwed's
+                    Official Account
+                  </span>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>Premium Design</span>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>360-Degree Virtual Reception</span>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>360-Degree Virtual Memories</span>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>Digital Gifts</span>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>360-Degree Live Streaming Wedding</span>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>360-Degree Photo & Video Documentation</span>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Image
+                    priority={true}
+                    src="/assets/tick-pricing.svg"
+                    alt="benefit"
+                    width={24}
+                    height={24}
+                  />
+                  <span>Lifetime Access</span>
+                </div>
               </div>
             </div>
             <button className="w-full text-xl border-2 border-black rounded text-center font-medium py-4">
@@ -176,6 +206,52 @@ function Pricing({
             </button>
           </div>
         </Fade>
+      </div>
+      <div
+        className={`grid grid-cols-2 lg:grid-cols-4 gap-4 text-white ${lora.className} text-lg lg:text-2xl`}
+      >
+        <div className="flex flex-col justify-between gap-8 p-4 lg:p-8 bg-[#F66F6F] rounded-lg">
+          <Image
+            src="/assets/addons-1.svg"
+            alt="add ons"
+            width={60}
+            height={60}
+          />
+          <span>Additional Invited Guests</span>
+          <span>Rp1.500</span>
+        </div>
+        <div className="flex flex-col justify-between gap-8 p-4 lg:p-8 bg-[#F66F6F] rounded-lg">
+          <Image
+            src="/assets/addons-2.svg"
+            alt="add ons"
+            width={60}
+            height={60}
+          />
+          <span>Lifetime Access Upgrade</span>
+          <span>Rp500.000</span>
+        </div>
+        <div className="flex flex-col justify-between gap-8 p-4 lg:p-8 bg-[#F66F6F] rounded-lg">
+          <Image
+            src="/assets/addons-3.svg"
+            alt="add ons"
+            width={60}
+            height={60}
+            className="lg:w-[80px]"
+          />
+          <span>360-Degree Live Streaming Wedding</span>
+          <span>Rp5.000.000</span>
+        </div>
+        <div className="flex flex-col justify-between gap-8 p-4 lg:p-8 bg-[#F66F6F] rounded-lg">
+          <Image
+            src="/assets/addons-4.svg"
+            alt="add ons"
+            width={60}
+            height={60}
+            className="lg:w-[80px]"
+          />
+          <span>360-Degree Photo & Video Documentation</span>
+          <span>Rp4.000.000</span>
+        </div>
       </div>
     </section>
   );
